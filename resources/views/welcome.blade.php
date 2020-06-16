ad10935997746ad50a805b062705176c4310a3aa
sadiqumar18 <sadiqumar18@gmail.com>
2020-06-16 17:36:07 +0100

    added landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,153 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
 
-        <title>Laravel</title>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel='icon' href='landing/images/favicon.ico' type='image/x-icon' />
+	<title>ZealVend</title><!-- Google font file. If you want you can change. -->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,900" rel="stylesheet">
+	<!-- Fontawesome font file css -->
+	<link rel="stylesheet" type="text/css" href="landing/css/css-font-awesome.min.css">
+	<!-- Animate css file for css3 animations. for more : https://daneden.github.io/animate.css -->
+	<!-- Only use animate action. If you dont use animation, you don't have to add.-->
+	<link rel="stylesheet" type="text/css" href="landing/css/css-animate.css">
+	<!-- Template global css file. Requared all pages -->
+	<link rel="stylesheet" type="text/css" href="landing/css/css-global.style.css">
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<body>
+	<!-- <header>
+			<a class="go-back-link" href="javascript:history.back();">
+				<i class="fa fa-arrow-left">
+				</i>
+			</a>
+		</header> -->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+	<!-- <div class="wrapper with-bg-image"> -->
+	<!-- <a class="go-back-link" href="javascript:history.back();">
+			<i class="fa fa-arrow-left">
+			</i>
+		</a> -->
+	<!-- <div class="nav-menu">
+			<nav class="menu"><div class="nav-header">
+		<a href="home.html"><img src="images/img-app-logo.png" width="160"></a>
+		</div> -->
+	<!-- Template logo end -->
+	<!-- Menu navigation start -->
+	<!-- <div class="nav-container">
+					<ul class="main-menu"><li class="active">
+						<a href="home.html"><i class="fa fa-home"></i> Home</a>
+						<a href="signup.html" data-loader="show"><i class="fa fa-th-large"></i> Get Started</a>
+						<a href="buydata.html" data-loader="show"><i class="fa fa-th-large"></i> Buy Data</a>
+						<a href="profile.html" data-loader="show"><i class="fa fa-th-large"></i> Profile</a>
+						<a href="contact.html" data-loader="show"><i class="fa fa-th-large"></i> Contact</a>
+							
+						</li>
+						<!-- <li>
+							<a href="javascript:void(0);"><i class="fa fa-sign-in"></i> Entry <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="login.html" data-loader="show">Login</a></li>
+								<li><a href="signup.html" data-loader="show">Register</a></li>
+							</ul></li>
+						<li> -->
+	<!-- <a href="javascript:void(0);"><i class="fa fa-file-image-o"></i> Gallery <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="album.html" data-loader="show">Albums</a></li>
+								<li><a href="gallery.html" data-loader="show">Gallery</a></li>
+							</ul></li>
+						<li>
+						<a href="#"><i class="fa fa-file-text-o"></i> Blog <span class="fa fa-angle-down"></span></a> -->
+	<!-- <ul><li><a href="post-list-1.html" data-loader="show">List 1</a></li>
+								<li><a href="post-list-2.html" data-loader="show">List 2</a></li>
+								<li><a href="post-detail.html" data-loader="show">Detail</a></li>
+								<li><a href="post-author.html" data-loader="show">Author</a></li>
+							</ul></li> -->
+	<li>
+		<!-- <a href="#"><i class="fa fa-shopping-basket"></i> Shop <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="product-list.html" data-loader="show">Product List</a></li>
+								<li><a href="product-detail.html" data-loader="show">Product Detail</a></li>
+								<li><a href="product-basket.html" data-loader="show">Basket</a></li>
+							</ul></li>
+						<li>
+							<a href="javascript:void(0);"><i class="fa fa-magic"></i> Wizards <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="wizard-default.html" data-loader="show">Wizard Default</a></li>
+								<li><a href="wizard-fullscreen.html" data-loader="show">Wizard Fullscreen</a></li>
+							</ul></li> -->
+	<li>
 
-            .full-height {
-                height: 100vh;
-            }
+	</li>
+	<li>
+		<!-- <a href="#"><i class="fa fa-table"></i> Components <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="tab-bottom.html" data-loader="show">Tab (Bottom)</a></li>
+								<li><a href="tab-top.html" data-loader="show">Tab (Top)</a></li>
+								<li><a href="accordion.html" data-loader="show">Accordion</a></li>
+								<li><a href="popup.html" data-loader="show">Popup Modal</a></li>
+								<li><a href="checkbox-list.html" data-loader="show">Check List</a></li>
+								<li><a href="link-list.html" data-loader="show">Link List</a></li>
+								<li><a href="link-list-two-column.html" data-loader="show">Two Column Links</a></li>
+							</ul></li>
+							<li>
+							<a href="#"><i class="fa fa-file-text-o"></i> Pages <span class="fa fa-angle-down"></span></a>
+							<ul><li><a href="profile.html" data-loader="show">User Profile</a></li>
+								<li><a href="search-popup.html" data-loader="show">Search Popup</a></li>
+								<li><a href="search-result.html" data-loader="show">Search Results</a></li>
+								<li><a href="contact.html" data-loader="show">Contact</a></li>
+								<li><a href="blank.html" data-loader="show">Blank Page</a></li> -->
+		</ul>
+	</li>
+	</ul>
+	</div> -->
+	<!-- Menu navigation end -->
+	</nav>
+	</div>
+	<div class="wrapper-inline">
+		<!-- Header area start -->
+		<header>
+			<!-- extra class no-background --><a class="go-back-link" href="javascript:history.html"><i
+					class=""></i></a>
+			<h1 class="page-title">ZealVend</h1>
+			<div class="">
+				<em>login</em>
+				<em>register</em>
+				<em>dashboard</em>
+			</div>
+		</header><!-- Header area end -->
+		<!-- Page content start -->
+		<main>
+			<section class="container">
+				<div class="animated-text vertical-center zero-opacity">
+					<h1>
+						<span class="text1 block txt-light" data-transation="fadeInDown" data-start-time="0"><img
+								src="landing/images/logo.png" width="160"></span>
+						<span class="text4 block txt-extra-bold txt-red" data-transation="zoomInDown"
+							data-start-time="500">Get Data</span>
+						<span class="text5 block txt-extra-bold txt-red" data-transation="zoomInDown"
+							data-start-time="600">AT AFFORDABLE</span>
+						<span class="text6 block txt-extra-bold txt-red" data-transation="zoomInDown"
+							data-start-time="700">PRICES</span>
+						<span class="text7 block txt-light" data-transation="fadeInUp" data-start-time="1000">Instant
+							crediting</span>
+						<br>
+						<div class="text-center" data-transation="tada" data-start-time="2000">
+							<a href="https://www.portal.zealvend.com/" class="button circle blue">Get Started</a>
+						</div>
+					</h1>
+				</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+			</section>
+		</main><!-- Page content end -->
 
-            .position-ref {
-                position: relative;
-            }
+		<!--Page loader DOM Elements. Requared all pages-->
+		<div class="sweet-loader">
+			<div class="box">
+				<div class="circle1"></div>
+				<div class="circle2"></div>
+				<div class="circle3"></div>
+			</div>
+		</div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		<!-- JQuery library file. requared all pages -->
+		<script src="landing/js/4680-js-jquery-3.2.1.min.js"></script><!-- Template global script file. requared all pages -->
+		<script src="landing/js/2756-js-global.script.js"></script>
+</body>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
