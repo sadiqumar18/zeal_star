40cdbb6f464b6266380e976b93b5c337e031b445
sadiqumar18 <sadiqumar18@gmail.com>
2020-07-17 11:18:15 +0100

    add ussd to table

--- app/DataProduct.php
+++ app/DataProduct.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class DataProduct extends Model
 {
     protected $guarded = [];
+
+    protected $casts = [
+        'ussd_param' => 'array', // Will convarted to (array)  
+    ];
+
+
+
 }
