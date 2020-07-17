29ad4c37f6266d123a45bf4756f57c835483f5ce
sadiqumar18 <sadiqumar18@gmail.com>
2020-07-17 21:13:41 +0100

    changes to migration

--- database/migrations/2020_07_17_042006_add_ussd_params_field.php
+++ database/migrations/2020_07_17_042006_add_ussd_params_field.php
@@ -15,6 +15,8 @@ class AddUssdParamsField extends Migration
     {
         Schema::table('data_products', function (Blueprint $table) {
             $table->json('ussd_param')->after('code')->nullable();
+            $table->dropUnique('data_products_code_unique');
+            $table->boolean('is_suspended')->default(false);
         });
     }
 
@@ -27,6 +29,7 @@ class AddUssdParamsField extends Migration
     {
         Schema::table('data_products', function (Blueprint $table) {
             $table->dropColumn('ussd_param');
+            $table->dropColumn('is_suspended');
         });
     }
 }
