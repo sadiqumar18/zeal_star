18fc485f95255b12a0c95d03120eb99e67dc61dc
sadiqumar18 <sadiqumar18@gmail.com>
2020-08-27 08:06:07 +0100

    refactor webhook

--- app/Api/V1/Controllers/WebhookController.php
+++ app/Api/V1/Controllers/WebhookController.php
@@ -23,382 +23,261 @@ class WebhookController extends Controller
 
 
 
-        
-       
-        $check_retry = (strpos($request->ref_code, 'retry') !== false);
-        
-        if($check_retry){
+        $dataController = new  DataTransactionController;
 
-            $ref_code = explode('?',$request->ref_code)[1];
-            
-        }else{
+        $check_retry = (strpos($request->ref_code, 'retry') !== false);
+
+        if ($check_retry) {
+
+            $ref_code = explode('?', $request->ref_code)[1];
+        } else {
 
             $ref_code = $request->ref_code;
-
         }
-
-
-      
-        
-
 
         $message = $request->message;
 
-        //check successfully
-        $check_successfully = (strpos($request->message, 'successfully') !== false);
 
-        $check_successful = (strpos($request->message, 'successful') !== false);
+        $check_ignore_case =  collect(config('webhook.ignore_clause'))->contains(function ($value, $key) use ($message) {
+            return (strpos($message, $value) !== false);
+        });
 
-        $check_etisalat_failed = (strpos($request->message, 'Sorry Operation failed') !== false);
-
-        $check_airtel_failed = (strpos($request->message, 'cannot be processed') !== false);
-
-        $check_etisalat_wait = (strpos($request->message,'please wait for a confirmation SMS thank you') !==false);
-
-        $check_ussd_time_out = (strpos($request->message,'Ussd timeout occurred!') !== false);
-
-        $connection_mmi = (strpos($request->message,'Connection problem or invalid MMI code.') !== false);
-
-        $fall_back = (strpos($request->message,'SHARE') !== false);
-
-        $fall_sorry = (strpos($request->message,'Sorry') !== false);
-
-        $check_oops = (strpos($request->message,'Oops, looks like the code you used was incorrect. Please check and try again.') !== false);
-
-        $enter_number = (strpos($request->message,"Enter Recipient's numbe") !== false);
-
-        $invalid_input = (strpos($request->message,"Invalid input provided.") !== false);
-
-        $invalid_msisdn = (strpos($request->message,"Invalid msisdn provided") !== false);
-
-        $wrong_number = (strpos($request->message,"You are not sending to valid MTN number.") !== false);
-
-        $invalid_input2 = (strpos($request->message,"Yello, invalid input entered . Please check and try again.") !== false);
-
-        $system_busy = (strpos($request->message,"System is busy. Please try later.") !== false); 
-
-
-        $mmi_error = (strpos($request->message,"MMI complete.") !== false);
-
-        $unknown_application = (strpos($request->message,"UNKNOWN APPLICATION") !== false);
-
-
-        $customer_service = (strpos($request->message,"Dear Customer, Service is currently unavailable.") !== false);
-
-        $share_limit = (strpos($request->message,"You have reached your SME data share limit.") !== false);
-
-
-        $carrier_info = (strpos($request->message,"Carrier info") !== false);
-
-        $insufiicient_data = (strpos($request->message,"You don't have sufficient data to share.") !== false);
-
-        $etisalat_operation_failed = (strpos($request->message,"Sorry Operation failed , Please try again later") !== false);
-
-       $etisalat_insuficient_balance = (strpos($request->message,"SORRY!Insufficient credit balance for the plan you want to buy.Please recharge your line or you can simply Borrow Data. To Borrow Data now, just dial *321#") !== false);
-
-       $glo_wrong_number = (strpos($request->message,"Sorry, you are not gifting to a valid Globacom user.") !== false);
-    
-       
-       if($wrong_number){
-
-            $dataController = new  DataTransactionController;
-
-           
-
-            $dataController->reverseTransaction($ref_code);
-
+        if ($check_ignore_case) {
+            return response()->json(['status' => 'success']);
         }
 
 
-        if($enter_number or $invalid_msisdn or $system_busy or $connection_mmi or $check_oops or $share_limit or $etisalat_operation_failed or $glo_wrong_number ){
 
-            $dataController = new  DataTransactionController;
+        $check_reverse_case =  collect(config('webhook.reverse_clause'))->contains(function ($value, $key) use ($message) {
+            return (strpos($message, $value) !== false);
+        });
 
+
+        if ($check_reverse_case) {
+            $dataController->reverseTransaction($ref_code);
+            return response()->json(['status' => 'reverse']);
+        }
+
+
+
+        $check_retry_case = collect(config('webhook.retry_clause'))->contains(function ($value, $key) use ($message) {
+            return (strpos($message, $value) !== false);
+        });
+
+        if ($check_retry_case) {
             $allow_transaction = Setting::find(1)->allow_transaction;
 
             if ($allow_transaction == 'on') {
                 $dataController->retry($ref_code);
             }
 
-           
 
+            return response()->json(['status' => 'retry']);
         }
 
-      
-        
 
 
-        if ( $check_etisalat_failed
-            or $check_airtel_failed
-            or $check_etisalat_wait
-            or $check_ussd_time_out
-            or $connection_mmi
-            or $check_oops
-            or $fall_back
-            or $enter_number
-            or $fall_sorry
-            or $invalid_input
-            or $invalid_msisdn
-            or $wrong_number
-            or $mmi_error
-            or $unknown_application
-            or $invalid_input2
-            or $system_busy
-            or $customer_service
-            or  $carrier_info
-            or $etisalat_insuficient_balance
-            or $insufiicient_data
-           ) {
-            return response()->json(['status' => 'success']);
-        }
+        $check_successful_case = collect(config('webhook.success_clause'))->contains(function ($value, $key) use ($message) {
+            return (strpos($message, $value) !== false);
+        });
 
-    
+
+
+
+
+
+
 
         switch ($ref_code) {
 
 
             case '131':
 
-                if (!$check_successfully) {
+                if (!$check_successful_case) {
                     return response()->json(['status' => 'success']);
                 }
 
-                $message = explode('.',$message)[0];
+                $message = explode('.', $message)[0];
 
 
-               $exploded_message = explode(' ', $message);  //preg_match_all('!\d+!', $message, $array);
+                $exploded_message = explode(' ', $message);  //preg_match_all('!\d+!', $message, $array);
 
-              
-              
                 //get number
                 preg_match_all('!\d+!', $message, $array);
 
                 $number = "0" . substr($array[0][1], 3, 12);
-                
+
                 $bundle = $exploded_message[6];
 
-               
+
                 $data_bundle = $this->getMtnBundle($bundle);
 
-            
+
                 $transaction = DataTransaction::whereNumber($number)->whereBundle($data_bundle)->whereStatus('processing')->first();
 
 
                 if ($transaction) {
-                    $this->updateDataAndSendWebhook($transaction,$message);
+                    $this->updateDataAndSendWebhook($transaction, $message);
                 }
 
                 return response()->json(['status' => 'success']);
 
                 break;
 
-        case '127':    
-            
-            if (!$check_successfully) {
-                return response()->json(['status' => 'success']);
-            }
+            case '127':
 
-           //get number
-           preg_match_all('!\d+!', $message, $array);
-
-          
-
-           $number = "0" . substr($array[0][1], 3, 12);
-
-
-            $transaction = DataTransaction::whereNumber($number)->where('network','GLO')->whereStatus('processing')->first();
-
-            if ($transaction) {
-                $this->updateDataAndSendWebhook($transaction,$message);
-            }
-
-            return response()->json(['status' => 'success']);
-
-        break;
-
-        case '9mobile':
-
-            if (!$check_successfully) {
-                return response()->json(['status' => 'success']);
-            }
-
-            $exploded_message = explode(' ', $message);
-
-            $remove_period = explode('.',$exploded_message[9]);
-
-         
-            $number = "0".$remove_period[0];
-
-          
-            $transaction = DataTransaction::whereNumber($number)->where('network','ETISALAT')->whereStatus('processing')->first();
-
-            if ($transaction) {
-                $this->updateDataAndSendWebhook($transaction,$message);
-            }
-
-            return response()->json(['status' => 'success']);
-
-        break;  
-        
-        case 'AirtelERC':
-
-
-            if (!$check_successful) {
-                return response()->json(['status' => 'success']);
-            }
-
-            $exploded_message = explode(' ',$message);
-
-            $number = $exploded_message[8];
-
-            $transaction = DataTransaction::whereNumber($number)->where('status','processing')->where('network','AIRTEL')->orderBy('id','DESC')->first();
-
-           
-            if (is_null($transaction)) {
-
-               $airtime_transaction = AirtimeTransaction::whereNumber($number)->where('network','AIRTEL')->orderBy('id','DESC')->first();
-              
-               $this->updateAirtimeAndSendWebhook($airtime_transaction,$message);
-            }
-
-            if ($transaction) {
-                $this->updateDataAndSendWebhook($transaction,$message);
-            }
-
-            return response()->json(['status' => 'success']);
-
-        break;
-
-        case 'MTN Topit':
-
-            $number = explode('To:', $message);
-
-
-            $number = "0" . substr($number[1], 4, 12);
-
-            $transaction = AirtimeTransaction::whereNumber($number)->orderBy('id','DESC')->first();
-
-
-            if ($transaction) {
-                $this->updateAirtimeAndSendWebhook($transaction,$message);
-             }
-
-             return response()->json(['status' => 'success']);
-
-
-        break;
-
- 
-        default:
-
-                $airtel_flag = (strpos($request->message, 'under process') !== false);
-
-                $airtel_flag2 = (strpos($request->message, 'Your request to recharge') !== false);
-
-                $topup_flag =  (strpos($request->message, 'topped up') !== false);
-
-                $check_mtn_new_success = (strpos($request->message, 'Dear Customer') !== false);
-
-                if ((strpos($request->message, 'SENT') !== false)
-                ) {
-                 return response()->json(['status' => 'success']);
-                }
-
-
-                if($check_mtn_new_success){
-                   
-                    $message = explode('.',$message)[0];
-
-            
-                    $this->ussdTransaction($ref_code,$message);
-
+                if (!$check_successful_case) {
                     return response()->json(['status' => 'success']);
-
-
                 }
 
-                if ($check_successfully) {
-                    $this->ussdTransaction($ref_code,$message);
+                //get number
+                preg_match_all('!\d+!', $message, $array);
+
+
+
+                $number = "0" . substr($array[0][1], 3, 12);
+
+
+                $transaction = DataTransaction::whereNumber($number)->where('network', 'GLO')->whereStatus('processing')->first();
+
+                if ($transaction) {
+                    $this->updateDataAndSendWebhook($transaction, $message);
+                }
+
+                return response()->json(['status' => 'success']);
+
+                break;
+
+            case '9mobile':
+
+                if (!$check_successful_case) {
+                    return response()->json(['status' => 'success']);
+                }
+
+                $exploded_message = explode(' ', $message);
+
+                $remove_period = explode('.', $exploded_message[9]);
+
+
+                $number = "0" . $remove_period[0];
+
+
+                $transaction = DataTransaction::whereNumber($number)->where('network', 'ETISALAT')->whereStatus('processing')->first();
+
+                if ($transaction) {
+                    $this->updateDataAndSendWebhook($transaction, $message);
+                }
+
+                return response()->json(['status' => 'success']);
+
+                break;
+
+            case 'AirtelERC':
+
+
+                if (!$check_successful_case) {
+                    return response()->json(['status' => 'success']);
+                }
+
+                $exploded_message = explode(' ', $message);
+
+                $number = $exploded_message[8];
+
+                $transaction = DataTransaction::whereNumber($number)->where('status', 'processing')->where('network', 'AIRTEL')->orderBy('id', 'DESC')->first();
+
+
+                if (is_null($transaction)) {
+
+                    $airtime_transaction = AirtimeTransaction::whereNumber($number)->where('network', 'AIRTEL')->orderBy('id', 'DESC')->first();
+
+                    $this->updateAirtimeAndSendWebhook($airtime_transaction, $message);
+                }
+
+                if ($transaction) {
+                    $this->updateDataAndSendWebhook($transaction, $message);
+                }
+
+                return response()->json(['status' => 'success']);
+
+                break;
+
+            case 'MTN Topit':
+
+                $number = explode('To:', $message);
+
+
+                $number = "0" . substr($number[1], 4, 12);
+
+                $transaction = AirtimeTransaction::whereNumber($number)->orderBy('id', 'DESC')->first();
+
+
+                if ($transaction) {
+                    $this->updateAirtimeAndSendWebhook($transaction, $message);
+                }
+
+                return response()->json(['status' => 'success']);
+
+
+                break;
+
+
+            default:
+
+
+
+                if ($check_successful_case) {
+
+                    $message = explode('.', $message)[0];
+
+                    $this->ussdTransaction($ref_code, $message);
 
                     return response()->json(['status' => 'success']);
                 }
 
-                if($airtel_flag){
 
-                    $this->ussdTransaction($ref_code,$message);
 
-                    return response()->json(['status' => 'success']);
-                }
-
-                if ($topup_flag) {
-                
-                $this->ussdTransaction($ref_code,$message);
+                $this->ussdTransaction($ref_code, $message);
 
                 return response()->json(['status' => 'success']);
 
-                }
 
-              
-
-                $this->ussdTransaction($ref_code,$message);
-
-                return response()->json(['status' => 'success']);
-
-                
-
-            break;
+                break;
+        }
+    }
 
 
 
+
+    private function ussdTransaction($ref_code, $message)
+    {
+        $transaction = DataTransaction::where('referrence', $ref_code)->whereStatus('processing')->first();
+
+
+        if (is_null($transaction)) {
+
+            $airtime_transaction =   AirtimeTransaction::where('referrence', $ref_code)->first();
+
+            if ($airtime_transaction) {
+
+                $this->updateAirtimeAndSendWebhook($airtime_transaction, $message);
+            }
         }
 
+        if ($transaction) {
 
-
-
-
-
-
-
-
-    }
-
-
-
-
-    private function ussdTransaction($ref_code,$message)
-    {
-             $transaction = DataTransaction::where('referrence', $ref_code)->whereStatus('processing')->first();
-
-
-            if(is_null($transaction)){
-
-                $airtime_transaction =   AirtimeTransaction::where('referrence', $ref_code)->first();
-
-                if($airtime_transaction){
-
-                $this->updateAirtimeAndSendWebhook($airtime_transaction,$message);
-
-                }
-
-
-            }
-
-            if($transaction){
-
-                $this->updateDataAndSendWebhook($transaction,$message);
-
-            }
+            $this->updateDataAndSendWebhook($transaction, $message);
+        }
     }
 
 
 
 
 
-    private function updateDataAndSendWebhook($transaction,$message)
+    private function updateDataAndSendWebhook($transaction, $message)
     {
         $transaction->update(['status' => 'successful', 'message' => $message]);
 
-       
+
         $user = $transaction->user;
 
         if (!is_null($user->webhook_url) or !empty($user->webhook_url)) {
@@ -407,7 +286,7 @@ class WebhookController extends Controller
     }
 
 
-    private function updateAirtimeAndSendWebhook($transaction,$message)
+    private function updateAirtimeAndSendWebhook($transaction, $message)
     {
         $transaction->update(['status' => 'successful', 'message' => $message]);
 
@@ -443,28 +322,4 @@ class WebhookController extends Controller
 
         return $bundle;
     }
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
---
+++ config/webhook.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+
+return [
+
+
+    "reverse_clause" => [
+        "You are not sending to valid MTN number.",
+        "Sorry, you are not gifting to a valid Globacom user."
+    ],
+
+    "ignore_clause" => [
+        "SENT",
+        "Sorry Operation failed",
+        "cannot be processed",
+        "please wait for a confirmation SMS thank you",
+        "Ussd timeout occurred!",
+        "Invalid input provided.",
+        "MMI complete.",
+        "UNKNOWN APPLICATION",
+        "Dear Customer, Service is currently unavailable.",
+        "Carrier info",
+        "You don't have sufficient data to share."
+    ],
+
+    "retry_clause" => [
+        "Connection problem or invalid MMI code.",
+        "Oops, looks like the code you used was incorrect. Please check and try again.",
+        "Enter Recipient's numbe",
+        "Invalid msisdn provided",
+        "You are not sending to valid MTN number.",
+        "Yello, invalid input entered . Please check and try again.",
+        "System is busy. Please try later.",
+        "You have reached your SME data share limit.",
+        "Sorry Operation failed , Please try again later",
+        "SORRY!Insufficient credit balance for the plan you want to buy.Please recharge your line or you can simply Borrow Data. To Borrow Data now, just dial *321#",
+    ],
+    "success_clause"=>[
+        "successfully",
+        "successful",
+        "under process",
+        "topped up"
+    ]
+
+];
