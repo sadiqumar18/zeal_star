b4a64d49fd3e82071c5c6b4b6b8742e34e9f3833
sadiqumar18 <sadiqumar18@gmail.com>
2020-09-21 11:59:35 +0000

    add to retry clause

--- config/webhook.php
+++ config/webhook.php
@@ -39,7 +39,8 @@ return [
         "Sorry Operation failed , Please try again later",
         "SORRY!Insufficient credit balance for the plan you want to buy.Please recharge your line or you can simply Borrow Data. To Borrow Data now, just dial *321#",
         "You have entered invalid PIN.",
-        "Sorry for the inconvenience Please try after some time"
+        "Sorry for the inconvenience Please try after some time",
+        "Sorry Operation failed , Please try again later"
     ],
     "success_clause"=>[
         "successfully",
