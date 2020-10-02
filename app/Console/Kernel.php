8cb6aa7b97a2c1cfbbb6fcb6929de926f1d7f683
sadiqumar18 <33685006+sadiqumar18@users.noreply.github.com>
2020-10-02 16:31:44 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     {
 
        
-        $schedule->command('retry:data 10 mtn 50')->everyTenMinutes();
+        $schedule->command('retry:data 10 mtn 10')->everyFifteenMinutes();
         
         $schedule->command('telescope:prune --hours=2')->hourly();
         $schedule->command('telescope:clear')->hourly();
